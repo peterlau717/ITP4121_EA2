ffc1124f73c9118da082ac91ea5d9e43c28c4f57
peterlau717 <97721723+peterlau717@users.noreply.github.com>
2023-04-20 12:02:52 +0800

    Add files via upload

---
+++ versions.tf
@@ -0,0 +1,13 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.1.0"
+    }
+  }
+  required_version = ">= 0.14"
+}
+
